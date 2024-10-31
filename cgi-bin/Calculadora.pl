116f27a8a4ea8ce2dd2310be567c6cff82e9b040
SrEstrada <sestradaa@unsa.edu.pe>
2024-10-31 04:18:42 -0500

    Agregue unas cuantas cosas en el html de insdex además de corregir un copiado de un código antiguo que borro parte en sí del html y agregue codigo en el pl para que reciba datos y muestre la operación ingresada.

--- cgi-bin/Calculadora.pl
+++ cgi-bin/Calculadora.pl
@@ -4,3 +4,13 @@ use warnings;
 use CGI ':standard';
 
 print header();
+
+# Configuración para recibir la operación
+my $query = CGI->new;
+my $operacion = $query->param('operacion');
+
+# Mostrar un mensaje de prueba
+print "<html><body>";
+print "<h1>Resultado</h1>";
+print "<p>Operación recibida: $operacion</p>";
+print "</body></html>";
