3c9bf5bf970ed525e9c5d036c3481d5f954f5fd0
SrEstrada <sestradaa@unsa.edu.pe>
2024-10-31 05:15:58 -0500

    Al fin implementamos la lógica de cálculo en Calculadora.pl para las operaciones de suma y resta cuando la expresión es válida.

--- cgi-bin/Calculadora.pl
+++ cgi-bin/Calculadora.pl
@@ -11,12 +11,23 @@ my $operacion = $query->param('operacion');
 print "<html><body>";
 print "<h1>Resultado</h1>";
 
+# Verificamos que solo se usen caracteres válidos
 if ($operacion =~ /^[\d+\-*\/\(\)\s]+$/) {
+    # Expresión regular para identificar números y operadores básicos
     if ($operacion =~ /^(\d+)([+\-*\/])(\d+)$/) {
         my ($num1, $op, $num2) = ($1, $2, $3);
-        print "<p>Primer operando: $num1</p>";
-        print "<p>Operador: $op</p>";
-        print "<p>Segundo operando: $num2</p>";
+        my $resultado;
+
+        # Realizar la operación de acuerdo al operador
+        if ($op eq '+') {
+            $resultado = $num1 + $num2;
+        } elsif ($op eq '-') {
+            $resultado = $num1 - $num2;
+        }
+
+        # Mostrar el resultado
+        print "<p>Operación: $operacion</p>";
+        print "<p>Resultado: $resultado</p>";
     } else {
         print "<p>Operación inválida.</p>";
     }
