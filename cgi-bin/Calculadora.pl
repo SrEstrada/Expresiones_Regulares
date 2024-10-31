1a46b3b50cc7db5e2cb7169a520f49c2db6ef354
SrEstrada <sestradaa@unsa.edu.pe>
2024-10-31 04:27:21 -0500

    En este punto en el pl empezamos a utilizar las expresiones regulares para trabajar y ponemos los operadores básicos como suma, multiplicación, resta y división

--- cgi-bin/Calculadora.pl
+++ cgi-bin/Calculadora.pl
@@ -5,12 +5,20 @@ use CGI ':standard';
 
 print header();
 
-# Configuración para recibir la operación
 my $query = CGI->new;
 my $operacion = $query->param('operacion');
 
-# Mostrar un mensaje de prueba
 print "<html><body>";
 print "<h1>Resultado</h1>";
-print "<p>Operación recibida: $operacion</p>";
+
+# Expresión regular para identificar números y operadores básicos
+if ($operacion =~ /^(\d+)([+\-*\/])(\d+)$/) {
+    my ($num1, $op, $num2) = ($1, $2, $3);
+    print "<p>Primer operando: $num1</p>";
+    print "<p>Operador: $op</p>";
+    print "<p>Segundo operando: $num2</p>";
+} else {
+    print "<p>Operación inválida.</p>";
+}
+
 print "</body></html>";
